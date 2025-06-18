7f2b1790c50b074b0636711c06e6077f55c5dce3
huy.hoang <huy.hoang@AL03048503.local>
2025-06-18 23:26:02 +0700

    .

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -28,7 +28,6 @@ const HomePage: React.FC = () => {
           <StatsSection />
           <ServicesSection />
           <WorkflowSection />
-          <TeamSection />
           <ProjectDiarySection />
           <TestimonialSliderSection />
           <ConsultationFormSection />
