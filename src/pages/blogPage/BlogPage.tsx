31c128421cf5a11d9d4f22df3f8bdc77b77072e0
huy.hoang <huy.hoang@192.168.1.43.non-exists.ptr.local>
2025-06-26 00:46:23 +0700

    Updates

--- src/pages/blogPage/BlogPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./BlogPage.css";
-
-const BlogPage: React.FC = () => {
-  return <div></div>;
-};
-
-export default BlogPage;
--- src/pages/servicePage/ServicePage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ServicePage.css";
-
-const ServicePage: React.FC = () => {
-  return <div></div>;
-};
-
-export default ServicePage;
